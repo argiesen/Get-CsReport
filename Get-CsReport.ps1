b333f72c91e30c8fbaf960871b4b2e88f43dbb99
argiesen <argiesen@gmail.com>
2017-11-07 23:31:10 -0800

    Error suppression

--- Get-CsReport.ps1
+++ Get-CsReport.ps1
@@ -491,7 +491,7 @@ foreach ($site in $sites){
 					
 					#Collect QoS policies
 					$qosRegPolicies = Invoke-Command -ComputerName $server.Server -ScriptBlock {
-						return Get-ChildItem -Path HKLM:\Software\Policies\Microsoft\Windows\QoS | ForEach-Object {Get-ItemProperty $_.PSPath}
+						return Get-ChildItem -Path HKLM:\Software\Policies\Microsoft\Windows\QoS -ErrorAction SilentlyContinue | ForEach-Object {Get-ItemProperty $_.PSPath}
 					}
 					
 					#Variable for QoS status for modalities
@@ -581,14 +581,14 @@ foreach ($site in $sites){
 					if ($server.Version -match "6.0|5.0"){
 						$dotNetEKUCheckIgnored = Invoke-Command -ComputerName $server.Server -ScriptBlock {
 							if (Get-HotFix KB4014514,KB401511,KB4014508,KB4014513,KB4014509,KB4014506,KB4014510,KB4014512,KB4014507,KB4019472 -ErrorAction SilentlyContinue){
-								return Get-ItemProperty "HKLM:\Software\Microsoft\.NETFramework\v4.0.30319\System.Net.ServicePointManager.RequireCertificateEKUs" | Get-Item | Select-Object * -ExpandProperty Property
+								return Get-ItemProperty "HKLM:\Software\Microsoft\.NETFramework\v4.0.30319\System.Net.ServicePointManager.RequireCertificateEKUs" -ErrorAction SilentlyContinue | Get-Item | Select-Object * -ExpandProperty Property
 							}
 						}
 						if ($dotNetEKUCheckIgnored -match "DATAMCUSVC.exe"){$server.DotNetEKUCheckIgnored = $true}
 					}else{
 						$dotNetEKUCheckIgnored = Invoke-Command -ComputerName $server.Server -ScriptBlock {
 							if (Get-HotFix KB4014514,KB401511,KB4014508,KB4014513,KB4014509,KB4014506,KB4014510,KB4014512,KB4014507,KB4019472 -ErrorAction SilentlyContinue){
-								return Get-ItemProperty "HKLM:\Software\Microsoft\.NETFramework\v2.0.50727\System.Net.ServicePointManager.RequireCertificateEKUs" | Get-Item | Select-Object * -ExpandProperty Property
+								return Get-ItemProperty "HKLM:\Software\Microsoft\.NETFramework\v2.0.50727\System.Net.ServicePointManager.RequireCertificateEKUs" -ErrorAction SilentlyContinue | Get-Item | Select-Object * -ExpandProperty Property
 							}
 						}
 						if ($dotNetEKUCheckIgnored -match "DATAMCUSVC.exe"){$server.DotNetEKUCheckIgnored = $true}
